cc71c09dd2feb19e748d5356287d04db65a92741
Rui Teixeira <ruiteixeira@minderacodeacademy.com>
2024-07-31 11:14:38 +0100

    Donation model and DTO structure update
    - Added `DonationCreateDTO` for creating donations
    - Moved `DonationNotFoundException` to `org.mindera.fur.code.exceptions.donation`
    - Added `InvalidDonationAmountException` and `InvalidDonationDateException`
    - Fixed validation annotations in `DonationCreateDTO` and `DonationDTO`
    - Refactored `DonationMapper` to use MapStruct for DTO and model conversion
    - Applied service-related updates in `DonationService` to handle the new DTOs and exceptions

--- src/main/java/org/mindera/fur/code/exceptions/donation/DonationNotFoundException.java
+++ src/main/java/org/mindera/fur/code/exceptions/donation/DonationNotFoundException.java
@@ -1,9 +1,10 @@
-package org.mindera.fur.code.exceptions;
+package org.mindera.fur.code.exceptions.donation;
 
-public class DonationNotFoundException extends RuntimeException{
+public class DonationNotFoundException extends RuntimeException {
     public DonationNotFoundException(String message) {
         super(message);
     }
+
     public DonationNotFoundException() {
         super("Donation not found");
     }
--- src/main/java/org/mindera/fur/code/service/DonationService.java
+++ src/main/java/org/mindera/fur/code/service/DonationService.java
@@ -1,13 +1,19 @@
 package org.mindera.fur.code.service;
 
-import org.mindera.fur.code.exceptions.DonationNotFoundException;
+import org.mindera.fur.code.dto.donation.DonationDTO;
+import org.mindera.fur.code.exceptions.donation.DonationNotFoundException;
+import org.mindera.fur.code.exceptions.donation.InvalidDonationAmountException;
+import org.mindera.fur.code.exceptions.donation.InvalidDonationDateException;
+import org.mindera.fur.code.mapper.DonationMapper;
 import org.mindera.fur.code.model.Donation;
 import org.mindera.fur.code.repository.DonationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Optional;
 
+@Service
 public class DonationService {
 
     private final DonationRepository donationRepository;
@@ -17,51 +23,105 @@ public class DonationService {
         this.donationRepository = donationRepository;
     }
 
-    public Donation save(Donation donation) {
+    /**
+     * Creates a new donation record based on the provided DonationDTO.
+     *
+     * <p>This method performs several validation checks on the provided DonationDTO:
+     * <ul>
+     *   <li>Ensures that the donation ID, total amount, date, pet ID, and person ID
+     *       are not null and that the total amount is greater than 0.</li>
+     *   <li>Validates that the donation date is in the future.</li>
+     *   <li>Ensures that the total donation amount is less than 999999.</li>
+     * </ul>
+     * If any of these conditions are not met, an appropriate exception is thrown.
+     *
+     * <p>After successful validation, the donation is mapped to a Donation model object
+     * and saved to the repository.
+     *
+     * @param donation the DonationDTO containing the donation details
+     * @return the saved Donation object
+     * @throws IllegalArgumentException       if any required fields are null or invalid
+     * @throws InvalidDonationDateException   if the donation date is not in the future
+     * @throws InvalidDonationAmountException if the donation amount is too large
+     */
+    public Donation createDonation(DonationDTO donation) {
         //TODO
-        // change to DTO
-        // change strings to other file
+        // change strings to enum
 
         if (donation.getId() == null ||
                 donation.getTotal() <= 0 ||
                 donation.getDate() == null ||
-                donation.getPet() == null ||
-                donation.getPerson() == null) {
+                donation.getPetId() == null ||
+                donation.getPersonId() == null) {
             throw new IllegalArgumentException("Donation has invalid data fields");
         }
 
         if (donation.getDate().getTime() < System.currentTimeMillis()) {
-            throw new IllegalArgumentException("Donation date must be in the future");
+            throw new InvalidDonationDateException("Donation date must be in the future");
         }
 
         if (donation.getTotal() >= 999999) {
-            throw new IllegalArgumentException("Donation total must be less than 999999");
+            throw new InvalidDonationAmountException("Donation total must be less than 999999");
         }
 
-        Donation savedDonation = donationRepository.save(donation);
+        Donation savedDonation = donationRepository.save(DonationMapper.INSTANCE.toModel(donation));
         return savedDonation;
-
     }
 
-    public Optional<Donation> get(long id) {
+    /**
+     * Retrieves a donation by its ID.
+     *
+     * <p>This method attempts to find a donation record in the repository using the provided ID.
+     * If the donation is found, it is mapped to a DonationDTO and returned.
+     * If the donation is not found, a {@code DonationNotFoundException} is thrown.
+     *
+     * @param id the ID of the donation to retrieve
+     * @return the DonationDTO representing the donation details
+     * @throws DonationNotFoundException if no donation with the specified ID is found
+     */
+    public DonationDTO getDonationById(long id) {
         Optional<Donation> donation = donationRepository.findById(id);
         if (donation.isEmpty()) {
             throw new DonationNotFoundException();
         }
-        return donation;
+        return DonationMapper.INSTANCE.toDTO(donation.get());
     }
 
-    public ArrayList<Donation> list() {
-        ArrayList<Donation> donations = new ArrayList<>();
-        donationRepository.findAll().forEach(donations::add);
+    /**
+     * Retrieves all donations from the repository.
+     *
+     * <p>This method fetches all donation records from the repository, maps each one to a
+     * DonationDTO, and collects them in an ArrayList which is then returned.
+     *
+     * @return an ArrayList of DonationDTO objects representing all donations
+     */
+    public ArrayList<DonationDTO> getAllDonations() {
+        ArrayList<DonationDTO> donations = new ArrayList<>();
+        donationRepository.findAll().forEach(donation -> donations.add(DonationMapper.INSTANCE.toDTO(donation)));
         return donations;
     }
 
-    public ArrayList<Donation> list(long id) {
-        ArrayList<Donation> donations = new ArrayList<>();
+    /**
+     * Retrieves all donations associated with a specific person ID from the repository.
+     *
+     * <p>This method first validates the provided person ID to ensure it is greater than zero.
+     * It then fetches all donation records from the repository, filters them by the provided
+     * person ID, maps each matching donation to a DonationDTO, and collects these DTOs in an
+     * ArrayList which is then returned.
+     *
+     * @param id the ID of the person whose donations are to be retrieved
+     * @return an ArrayList of DonationDTO objects representing all donations for the specified person
+     * @throws IllegalArgumentException if the provided ID is not valid
+     */
+    public ArrayList<DonationDTO> getAllDonationsById(long id) {
+        if (id <= 0) {
+            throw new IllegalArgumentException("Id must be valid");
+        }
+
+        ArrayList<DonationDTO> donations = new ArrayList<>();
         donationRepository.findAll().forEach(donation -> {
             if (donation.getPerson().getId() == id) {
-                donations.add(donation);
+                donations.add(DonationMapper.INSTANCE.toDTO(donation));
             }
         });
         return donations;
