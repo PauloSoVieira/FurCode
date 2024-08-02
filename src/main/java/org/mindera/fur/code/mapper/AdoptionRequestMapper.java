62456dd894d867af1a0c8806fa1fd5166481a33e
Rui Sousa <150975299+ruitiago23@users.noreply.github.com>
2024-08-02 12:33:36 +0100

    Create AdoptionRequest Mapper Interface & Repository

---
+++ src/main/java/org/mindera/fur/code/mapper/AdoptionRequestMapper.java
@@ -0,0 +1,23 @@
+package org.mindera.fur.code.mapper;
+
+import org.mapstruct.factory.Mappers;
+import org.mindera.fur.code.dto.adoptionRequest.AdoptionRequestCreationDTO;
+import org.mindera.fur.code.dto.adoptionRequest.AdoptionRequestDTO;
+import org.mindera.fur.code.model.AdoptionRequest;
+
+import java.util.List;
+
+public interface AdoptionRequestMapper {
+
+    AdoptionRequestMapper INSTANCE = Mappers.getMapper(AdoptionRequestMapper.class);
+
+    AdoptionRequestDTO toDTO(AdoptionRequest adoptionRequest);
+
+    AdoptionRequest toModel(AdoptionRequestDTO adoptionRequestDTO);
+
+    AdoptionRequest toModel(AdoptionRequestCreationDTO adoptionRequestCreationDTO);
+
+    List<AdoptionRequestDTO> toDTO(List<AdoptionRequest> adoptionRequest);
+
+    List<AdoptionRequest> toModel(List<AdoptionRequestDTO> adoptionRequestDTO);
+}
