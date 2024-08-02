3227e331b534f117c0e136d66ee2da162f2c36f0
Pedro Teixeira <pedro.teixeira@minderacodeacademy.com>
2024-08-02 18:53:56 +0100

    update: enhance PetController and update pet services
    - Added swagger annotations to PetController for improved API documentation
    - Updated PetCreateDTO, PetMapper, and PetService for better data handling

--- src/main/java/org/mindera/fur/code/mapper/PetMapper.java
+++ src/main/java/org/mindera/fur/code/mapper/PetMapper.java
@@ -1,6 +1,7 @@
 package org.mindera.fur.code.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 import org.mindera.fur.code.dto.pet.PetCreateDTO;
 import org.mindera.fur.code.dto.pet.PetDTO;
@@ -16,4 +17,6 @@ public interface PetMapper {
     Pet toModel(PetDTO petDTO);
 
     Pet toModel(PetCreateDTO petCreateDTO);
+
+    void updatePetFromDTO(PetCreateDTO dto, @MappingTarget Pet model);
 }
