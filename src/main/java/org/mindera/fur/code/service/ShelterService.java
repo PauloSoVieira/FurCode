20ce69add5f0c2e754454129e57b07fbb5eb2c99
Rui Teixeira <rui.teixeira@minderacodeacademy.com>
2024-08-06 12:31:10 +0100

    Hotfix (#24)
    - Import of Pet Service on Shelter Service was wrong

--- src/main/java/org/mindera/fur/code/service/ShelterService.java
+++ src/main/java/org/mindera/fur/code/service/ShelterService.java
@@ -3,11 +3,11 @@ package org.mindera.fur.code.service;
 import org.mindera.fur.code.dto.shelter.ShelterCreationDTO;
 import org.mindera.fur.code.dto.shelter.ShelterDTO;
 import org.mindera.fur.code.mapper.ShelterMapper;
-import org.mindera.fur.code.model.Pet;
 import org.mindera.fur.code.model.Shelter;
+import org.mindera.fur.code.model.pet.Pet;
 import org.mindera.fur.code.repository.PersonRepository;
-import org.mindera.fur.code.repository.PetRepository;
 import org.mindera.fur.code.repository.ShelterRepository;
+import org.mindera.fur.code.repository.pet.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +16,7 @@ import java.util.List;
 
 @Service
 public class ShelterService {
-    
+
     private ShelterRepository shelterRepository;
     private PersonRepository personRepository;
     private PetRepository petRepository;
