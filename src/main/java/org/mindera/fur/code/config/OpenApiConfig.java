5db3eeabbc2f7c91fb9d57696849c2977e0bb4c9
Rui Teixeira <rui.teixeira@minderacodeacademy.com>
2024-08-13 17:01:54 +0100

    OpenAPI config fix (#57)
    - The button to put the authentication token should now be available

--- src/main/java/org/mindera/fur/code/config/OpenApiConfig.java
+++ src/main/java/org/mindera/fur/code/config/OpenApiConfig.java
@@ -3,6 +3,8 @@ package org.mindera.fur.code.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,11 +14,20 @@ class OpenApiConfig {
     @Bean
     OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
+                .addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
                 .info(new Info()
                         .title("FurCode API")
-                        .description("To decide later")
+                        .description("Open Source Animal Shelter Manager")
                         .version("0.0.1")
                 );
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 }
