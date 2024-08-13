2279cedd9019e469e3137da9c3f241ddb3697a71
Paulo Vieira <paulo92vieira@gmail.com>
2024-08-13 10:37:23 +0100

    fixed conflits

--- src/main/java/org/mindera/fur/code/repository/form/FormFieldAnswerRepository.java
+++ src/main/java/org/mindera/fur/code/repository/form/FormFieldAnswerRepository.java
@@ -3,6 +3,8 @@ package org.mindera.fur.code.repository.form;
 import jdk.jfr.Registered;
 import org.mindera.fur.code.model.form.FormFieldAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
-@Registered
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface FormFieldAnswerRepository extends JpaRepository<FormFieldAnswer, Long> {
 }
--- src/main/java/org/mindera/fur/code/repository/formTest/FormFieldAnswerRepository.java
+++
@@ -1,8 +0,0 @@
-package org.mindera.fur.code.repository.formTest;
-
-import jdk.jfr.Registered;
-import org.mindera.fur.code.model.form.FormFieldAnswer;
-import org.springframework.data.jpa.repository.JpaRepository;
-@Registered
-public interface FormFieldAnswerRepository extends JpaRepository<FormFieldAnswer, Long> {
-}
