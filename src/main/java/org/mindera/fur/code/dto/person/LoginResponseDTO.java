79d1823aed2550b081dfe45593401f8192825aa7
Paulo Vieira <paulo92vieira@gmail.com>
2024-10-02 14:23:57 +0100

    Final do final meu (#123)
    * Changed token to have person details
    
    * Changed token to have person details

--- src/main/java/org/mindera/fur/code/dto/person/LoginResponseDTO.java
+++ src/main/java/org/mindera/fur/code/dto/person/LoginResponseDTO.java
@@ -2,6 +2,52 @@ package org.mindera.fur.code.dto.person;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "A login response")
-public record LoginResponseDTO(String token) {
+@Schema(description = "A response object for login, containing person details and an authentication token.")
+public class LoginResponseDTO {
+
+    @Schema(description = "Details of the person logging in.", required = true)
+    private PersonDTO person;
+
+    @Schema(description = "JWT token generated upon successful login.", example = "eyJhbGciOiJIUzI1NiIsInR5...")
+    private String token;
+
+    /**
+     * Creates a new login response.
+     *
+     * @param person The person details.
+     * @param token The authentication token.
+     */
+    private LoginResponseDTO(PersonDTO person, String token) {
+        this.person = person;
+        this.token = token;
+    }
+
+    /**
+     * Static factory method to create a new instance of LoginResponseDTO.
+     *
+     * @param person The person details.
+     * @param token The authentication token.
+     * @return A new instance of LoginResponseDTO.
+     */
+    public static LoginResponseDTO create(PersonDTO person, String token) {
+        return new LoginResponseDTO(person, token);
+    }
+
+    /**
+     * Returns the person details of the login response.
+     *
+     * @return The person details.
+     */
+    public PersonDTO getPerson() {
+        return person;
+    }
+
+    /**
+     * Returns the authentication token of the login response.
+     *
+     * @return The authentication token.
+     */
+    public String getToken() {
+        return token;
+    }
 }
--- src/main/java/org/mindera/fur/code/mapper/PersonMapper.java
+++ src/main/java/org/mindera/fur/code/mapper/PersonMapper.java
@@ -9,38 +9,43 @@ import org.mindera.fur.code.model.Person;
 
 import java.util.List;
 
-@Mapper
+@Mapper(componentModel = "spring")
 @Schema(description = "The person mapper")
 public interface PersonMapper {
 
     /**
      * Singleton instance of the mapper
      */
+
     PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
-    /**
-     * Converts a Person to a PersonDTO
-     **/
+
+/**
+ * Converts a Person to a PersonDTO
+ **/
     PersonDTO toDTO(Person person);
 
-    /**
-     * Converts a PersonDTO to a Person
-     **/
+/**
+ * Converts a PersonDTO to a Person
+ **/
+
     Person toModel(PersonDTO personDTO);
 
+/**
+ * Converts a PersonCreationDTO to a Person
+ **/
 
-    /**
-     * Converts a PersonCreationDTO to a Person
-     **/
     Person toModel(PersonCreationDTO personCreationDTO);
 
     /**
-     * Converts a PersonDTO to a Person
+     * Converts a list of PersonDTO to a list of Person
      **/
+
     List<Person> toModel(List<PersonDTO> personDTOS);
 
     /**
-     * Converts a Person to a PersonDTO
+     * Converts a list of Person to a list of PersonDTO
      **/
+
     List<PersonDTO> toDTO(List<Person> personCreationDTOS);
 }
