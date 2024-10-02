d69e169ff9aed84633749151c020acd1c9f98d05
Paulo Vieira <paulo92vieira@gmail.com>
2024-10-02 13:14:12 +0100

    Changed token to have person details

--- src/main/java/org/mindera/fur/code/dto/person/LoginResponseDTO.java
+++ src/main/java/org/mindera/fur/code/dto/person/LoginResponseDTO.java
@@ -3,5 +3,25 @@ package org.mindera.fur.code.dto.person;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "A login response")
-public record LoginResponseDTO(String token) {
+public class LoginResponseDTO {
+    private PersonDTO person;
+    private String token;
+
+    private LoginResponseDTO(PersonDTO person, String token) {
+        this.person = person;
+        this.token = token;
+    }
+
+    public static LoginResponseDTO create(PersonDTO person, String token) {
+        return new LoginResponseDTO(person, token);
+    }
+
+    // Getters
+    public PersonDTO getPerson() {
+        return person;
+    }
+
+    public String getToken() {
+        return token;
+    }
 }
--- src/main/java/org/mindera/fur/code/mapper/PersonMapper.java
+++ src/main/java/org/mindera/fur/code/mapper/PersonMapper.java
@@ -9,38 +9,19 @@ import org.mindera.fur.code.model.Person;
 
 import java.util.List;
 
-@Mapper
+@Mapper(componentModel = "spring")
 @Schema(description = "The person mapper")
 public interface PersonMapper {
 
-    /**
-     * Singleton instance of the mapper
-     */
     PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
-    /**
-     * Converts a Person to a PersonDTO
-     **/
     PersonDTO toDTO(Person person);
 
-    /**
-     * Converts a PersonDTO to a Person
-     **/
     Person toModel(PersonDTO personDTO);
 
-
-    /**
-     * Converts a PersonCreationDTO to a Person
-     **/
     Person toModel(PersonCreationDTO personCreationDTO);
 
-    /**
-     * Converts a PersonDTO to a Person
-     **/
     List<Person> toModel(List<PersonDTO> personDTOS);
 
-    /**
-     * Converts a Person to a PersonDTO
-     **/
     List<PersonDTO> toDTO(List<Person> personCreationDTOS);
 }
